31dc8ed940aacee5c9a0344559fbd7047b0daf62
obaykulov <kim.o.r@icloud.com>
2024-07-22 06:36:02 +1000

    Initial Commit

---
+++ HW10-iOS13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW10-iOS13
+//
+//  Created by Олег on 22.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
